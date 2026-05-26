c64998030082a325673091664044b6fd1052a34b
thememphis <couragememphis@gmail.com>
2026-05-26 06:03:33 +0100

    added assignment.js

--- assignment.js
+++ assignment.js
@@ -12,10 +12,15 @@ console.log(reversedName);
 
 // write a javaScript code to change the value of an array
 
-let fruits = ["apple", "orange", "banana"];
+let fruits = ["Apple", "Orange", "Banana"];
 fruits [1] = "watermelon";
 
 console.log(fruits);
+
+let man = ["Money", "Responsibility", "Rich"];
+man [2]= "Healthy";
+
+console.log(man);
 
 //.Write a javaScript code to find the factorials of a number using the consept
 // of recursion  
